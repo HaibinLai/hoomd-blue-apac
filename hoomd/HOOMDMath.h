98576f9b0bb9294dce7b58c24d7ee85b58f11ade
Haibin LAI <12211612@mail.sustech.edu.cn>
2024-10-07 09:26:42 +0800

    perf: using fast rsqrt algorithm
    by Haibin Lai in APAC 2024

--- hoomd/HOOMDMath.h
+++ hoomd/HOOMDMath.h
@@ -246,7 +246,20 @@ inline HOSTDEVICE float rsqrt(float x)
     return ::__frsqrt_rn(x);
 #endif
 #else
-    return 1.0f / ::sqrtf(x);
+    // return 1.0f / ::sqrtf(x);
+
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = x * 0.5F;
+    y  = x;
+    i  = * ( long * ) &y;                        // evil floating point bit level hacking
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+    return y;
 #endif
     }
 
